0249e27fcb448a9657b6009aad19676688afbeef
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2024-09-11 23:39:57 +0500

    Airbnb

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Airbnb from './Airbnb'; // Airbnb logo component
+import { CiGlobe } from "react-icons/ci";
+
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center p-6 ">
+      {/* Left Section - Logo */}
+      <div className="lg:ml-14 md:ml-9 ml-5">
+        <Airbnb />
+      </div>
+
+      {/* Center Section - Navigation Links */}
+      <div className="flex space-x-8 mr-3">
+        <a href="#" className="text-gray-800 hover:text-black">Stays</a>
+        <a href="#" className="text-gray-800 hover:text-black">Experience</a>
+      </div>
+
+      {/* Right Section - 'Airbnb your home' and Language Icon */}
+      <div className="flex items-center space-x-4 mr-14">
+        <a href="#" className="text-gray-800 hover:text-black">Airbnb your home</a>
+        <button className="text-gray-800 hover:text-black">
+          <i className=" text-black font-bold fas fa-globe"><CiGlobe size={20} /></i> {/* Assuming you're using Font Awesome for the language icon */}
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
